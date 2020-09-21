66323d8fdeff7bd832a09db617997c2cf46a08f7
pjy1368@konkuk.ac.kr <pjy1368@konkuk.ac.kr>
2020-09-21 22:52:39 +0900

    init

---
+++ 2020_TeamProject/Main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main() {
+	/*string out_line;
+	ofstream out("test.txt");
+
+	for (int i = 0; i < 3; i++) {
+		cin >> out_line;
+		out << out_line << endl;
+	}
+
+	out.close()*/
+	
+	/*string in_line;
+	ifstream in("test.txt");
+	while (getline(in, in_line)) {
+		cout << in_line << endl;
+	}
+	in.close();*/
+	
+	
+	//_mkdir("user");
+}
