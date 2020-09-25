fc4d1618ea555f09a5226f5c7540bf73d45e8faa
algoORgoal <qufstar46@gmail.com>
2020-09-25 18:32:36 +0900

    clean up folders

---
+++ 2020_TeamProject_ReeSeo/Main.cpp
@@ -0,0 +1,34 @@
+#include <filesystem>
+#include <iostream>
+#include <string>
+
+namespace fs = std::filesystem;
+using namespace std;
+
+int main() {
+	if (!fs::exists("./user")) {
+		cout << "계정을 새로 만드시겠습니까? (yes or no) : ";
+
+		string ans;
+		cin >> ans;
+
+		if (ans == "yes") {
+			fs::create_directory("./user");
+		}
+	}
+	else {
+		cout << "1. 계정 초기화\n";
+		cout << "2. 기존 계정 사용하기\n";
+
+		string ans;
+		cin >> ans;
+
+		if (ans == "1") {
+			fs::remove("./user");
+		}
+		else if (ans == "2") {
+
+		}
+	}
+
+}
