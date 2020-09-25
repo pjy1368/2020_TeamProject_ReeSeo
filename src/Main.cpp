82ca5a157825e50fce78915874638a5733d438e7
pjy1368@konkuk.ac.kr <basejin0810@naver.com>
2020-09-25 18:08:10 +0900

    user login

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,6 +1,34 @@
-#include<iostream> 
+#include <filesystem>
+#include <iostream>
+#include <string>
+
+namespace fs = std::filesystem;
+using namespace std;
 
 int main() {
-    printf("hello");
-    return 0;
+	if (!fs::exists("./user")) {
+		cout << "계정을 새로 만드시겠습니까? (yes or no) : ";
+
+		string ans;
+		cin >> ans;
+
+		if (ans == "yes") {
+			fs::create_directory("./user");
+		}
+	}
+	else {
+		cout << "1. 계정 초기화\n";
+		cout << "2. 기존 계정 사용하기\n";
+
+		string ans;
+		cin >> ans;
+
+		if (ans == "1") {
+			fs::remove("./user");
+		}
+		else if (ans == "2") {
+
+		}
+	}
+
 }
