964493bfc17472addcc05d093b60d47c5e5f445d
algoORgoal <qufstar46@gmail.com>
2020-10-02 13:44:23 +0900

    fix grammatical errors in string

--- src/Main.py
+++ src/Main.py
@@ -4,26 +4,26 @@ account = Account()
 
 if account.isMember():
     print("1. Clear account")
-    print("2. Use existing account")
+    print("2. Use an existing account")
 
     sel = input("=> ")
 
     if sel == "1":
         account.clear()
     elif sel == "2":
-        print("Move main prompt")
+        print("Move to main prompt")
     else:
         print("Invalid input")
 else:
     print("1. Create account")
-    print("2. Move title menu")
+    print("2. Move to title menu")
 
     sel = input("=> ")
 
     if sel == "1":
         account.create()
     elif sel == "2":
-        print("Move title menu")
+        print("Move to title menu")
     else:
         print("Invalid input")
 
