04eef1cc923c51b8b0848464b2805fb969fa30fa
algoORgoal <qufstar46@gmail.com>
2020-10-02 13:36:29 +0900

    place variables in initializer

--- src/Account.py
+++ src/Account.py
@@ -2,11 +2,12 @@ import os
 import shutil
 
 class Account:
-    name = None
-    sex = None
-    age = None
-    height = None
-    weight = None
+    def __init__(self):
+        self.name = None
+        self.sex = None
+        self.age = None
+        self.height = None
+        self.weight = None
 
     # Does the account exist?
     def isMember(self):
