0fc2330b425f5727427f41b261facae99649b407
jaehoon-jo <chojh922@gmail.com>
2020-10-24 15:07:18 +0900

    change startScreen

--- src/StartScreen.py
+++ src/StartScreen.py
@@ -1,2 +1,24 @@
+import ctypes
+import os
+import sys
+
+def gotoxy(x,y):
+    return ctypes.windll.kernel32.SetConsoleCursorPosition(ctypes.windll.kernel32.GetStdHandle(-11),(((y&0xFFFF)<<0x10)|(x&0xFFFF)))
+
 def startScreen():
-    print("This is startScreen")
+    os.system('cls')
+    gotoxy(30,5)
+    print("<Exercise Objective Setting and Achievement Program>")
+    gotoxy(70,16)
+    print("201711370  Kang\tYunseok")
+    gotoxy(70,17)
+    print("201911156  Kim\tByeolchan")
+    gotoxy(70,18)
+    print("201911175  Park\tJinyoung")
+    gotoxy(70,19)
+    print("201911192  Yang\tSukjoon")
+    gotoxy(70,20)
+    print("201910514  Jo\tJaehun")
+    input()
+    os.system('cls')
+    
