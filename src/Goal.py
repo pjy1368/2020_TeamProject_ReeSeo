e6f139fa47b078501afac4258a3a36b3f072399b
pjy1368@konkuk.ac.kr <basejin0810@naver.com>
2020-10-25 17:11:30 +0900

    add input inspection in Goal.py

--- src/Goal.py
+++ src/Goal.py
@@ -1,8 +1,9 @@
 import os
+import re
 class Goal:
     def __init__(self, account):
         if self.isGoal():
-            filePath = "./user/profile.txt"
+            filePath = "./user/goal.txt"
             f = open(filePath, "r")
             s = f.readlines()
         
@@ -20,21 +21,31 @@ class Goal:
 
     # Set goal.
     def setGoal(self, account):
-        filePath = "./user/profile.txt"
+        filePath = "./user/goal.txt"
         f = open(filePath, "w")
         while True:
-            print("<Setting goal term>")
+            print("<Setting goal>")
             print("Enter the goal term")
             print("(Enter a natural number from 1 to 30 for the goal term) : ")
-            term = int(input("-> "))
-            if (type(term) is int and term > 0):
-                if (term >= 1 and term <= 30):
-                    f.write(str(term) + "\n")
-                    break
-                else:
-                    print("Enter a natural number from 1 to 30 for the goal term.")
-            else:
-                print("Term must be entered as a natural number.")
+            term = input("-> ")
+
+            if len(term) < 1 or len(term) > 2:
+                print("Enter a natural number from 1 to 30 for the goal term.")
+                input()
+                os.system('cls')
+                continue
+
+            p = re.search(r'^[0-9]{1,2}$', term)
+
+            if not p:
+                print("Enter a natural number from 1 to 30 for the goal term.")
+                input()
+                os.system('cls')
+                continue
+            
+            f.write(term + "\n")
+            os.system('cls')
+            break
 
         maxCalories = 0
         if account.gender == "Male":
@@ -42,11 +53,41 @@ class Goal:
         else:
             maxCalories = (float(account.weight) * 10) * 0.8
 
-        print("<Setting goal calories>")
-        print("The maximum daily calorie is " + str(maxCalories) + " kcal. Please enter the goal daily calorie.") 
-        print("The integer part of the value must be at least 2 digits and the decimal part must be 2 digits. At this point, the integer part must be positive")
-        calories = input("-> ")
-        f.write(calories + "\n")
+        while True:
+            print("<Setting goal>")
+            print("The maximum daily calorie is " + str(maxCalories) + " kcal. Please enter the goal daily calorie.") 
+            print("The integer part of the value must be at least 2 digits and the decimal part must be 2 digits. At this point, the integer part must be positive")
+            calories = input("-> ")
+
+            if len(calories) < 5:
+                print("The integer part of the value must be at least 2 digits and the decimal part must be at 2 digits.")
+                print("At this point, the integer part must be positive.")
+                input()
+                os.system('cls')
+                continue
+
+            if calories.find('.') == -1 or len(calories.split('.')[0]) < 2 or len(calories.split('.')[1]) != 2:
+                print("Illegal form of calories. Calories is consisted with at least 2 integer and 2 decimal classified with '.'.")
+                input()
+                os.system('cls')
+                continue
+
+            if int(calories.split('.')[0]) < 10:
+                print("Integer part of calories must be 10 or more.")
+                input()
+                os.system('cls')
+                continue
+
+            if float(calories) > maxCalories:
+                print("The maximum daily calorie must not be exceeded.")
+                input()
+                os.system('cls')
+                continue
+            
+            
+            f.write(str(float(calories)) + "\n")
+            os.system('cls')
+            break
 
         f.close()
         self.__init__(account)
@@ -57,7 +98,7 @@ class Goal:
 
     # Print goal.
     def view(self):
-        filePath = "./user/profile.txt"
+        filePath = "./user/goal.txt"
         f = open(filePath, 'r')
 
         print("<View goal>")
